26a73b6351950d6f240f7c87b89c56bf8c8b3ea2
Francisco Romano <franromano130@gmail.com>
2023-04-25 18:09:22 +0200

    progreso proyecto

---
+++ lessons/javascript-intro/script3.js
@@ -0,0 +1,20 @@
+
+
+let name = "carlitos"
+let lenguage = "english"
+
+switch(lenguage){
+    case "english":
+        console.log(`Hello ${name}`);
+        break;
+        case "spanish":
+            console.log(`Hola ${name}`)
+        break;
+    case "french":
+        console.log(`bonjour ${name}`)
+        break;
+        default:
+            console.log(`Nose quien eres`)
+
+}
+
